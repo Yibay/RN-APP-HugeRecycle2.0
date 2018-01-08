0939cbc25c112ca2188d28732166088ce41a1e8c
Yibay <514531536@qq.com>
2018-01-08 19:40:11 +0800

    定位页 iOS 获取GPS，转化成 BD-09坐标

--- app/containers/Location/CommunitySelector.js
+++ app/containers/Location/CommunitySelector.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ScrollView,TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, Text, ScrollView,TouchableWithoutFeedback, Alert } from 'react-native';
 
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
+import { wgs84togcj02, gcj02tobd09 } from 'coordtransform'; // 坐标转换
 
 
 import { setLocation } from '../../redux/actions/Location';
+import BackgroundGeolocation from 'react-native-mauron85-background-geolocation';
 
 
 class CommunitySelector extends Component{
@@ -50,6 +52,104 @@ class CommunitySelector extends Component{
         </View>
       </View>
     </View>);
+  }
+
+  componentDidMount(){
+
+    // 步骤1: 配置configure
+    BackgroundGeolocation.configure({
+      desiredAccuracy: 10,
+      stationaryRadius: 50,
+      distanceFilter: 50,
+      notificationTitle: 'Background tracking',
+      notificationText: 'enabled',
+      debug: true,
+      startOnBoot: false,
+      stopOnTerminate: false,
+      locationProvider: BackgroundGeolocation.DISTANCE_FILTER_PROVIDER, // location 供应商
+      interval: 10000,
+      fastestInterval: 5000,
+      activitiesInterval: 10000,
+      stopOnStillActivity: false,
+      maxLocations: 1, // db储存location数，就存1条id: 0的就好，节省存储空间
+      // url: 'http://192.168.81.15:3000/location',
+      httpHeaders: {
+        'X-FOO': 'bar'
+      }
+    });
+
+    BackgroundGeolocation.on('start', () => {
+      // 步骤3: 开启定位（监听）
+      console.log('[INFO] BackgroundGeolocation service has been started');
+    });
+
+    BackgroundGeolocation.on('authorization', (status) => {
+      // 步骤4: 授权定位（监听）
+      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+      if (status !== BackgroundGeolocation.AUTHORIZED) {
+        Alert.alert('Location services are disabled', 'Would you like to open location settings?', [
+          { text: 'Yes', onPress: () => BackgroundGeolocation.showLocationSettings() },
+          { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+        ]);
+      }
+    });
+
+    BackgroundGeolocation.on('location', (location) => {
+      // 步骤5: 获取定位结果 (监听)
+
+      // 地球坐标（WGS84）
+      console.log('------ on-location ------');
+      console.log('GPS坐标', location);
+      // 火星坐标 (GCJ-02)
+      let gcj02Location = wgs84togcj02(location.longitude, location.latitude);
+      console.log('火星坐标：', gcj02Location);
+      // 百度坐标 (BD-09)
+      let bd09Location = gcj02tobd09.apply(this, gcj02Location);
+      console.log('百度坐标：', bd09Location);
+
+      // 步骤6: 关闭 定位
+      BackgroundGeolocation.stop();
+    });
+
+    BackgroundGeolocation.on('stop', () => {
+      // 步骤6: 关闭 定位 (监听)
+      console.log('[INFO] BackgroundGeolocation service has been stopped');
+    });
+
+    BackgroundGeolocation.on('stationary', (stationaryLocation)=>{
+      console.log('on-stationaryLocation');
+      console.log(stationaryLocation);
+    });
+
+    BackgroundGeolocation.on('error', (error) => {
+      console.log('[ERROR] BackgroundGeolocation error:', error);
+    });
+
+    BackgroundGeolocation.on('background', () => {
+      console.log('[INFO] App is in background');
+    });
+
+    BackgroundGeolocation.on('foreground', () => {
+      console.log('[INFO] App is in foreground');
+    });
+
+    // 步骤2: 检测 状态
+    BackgroundGeolocation.checkStatus(status => {
+      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+      console.log('[INFO] BackgroundGeolocation service has permissions', status.hasPermissions);
+      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+      // you don't need to check status before start (this is just the example)
+      // 步骤3: 开启 定位
+      if (!status.isRunning) {
+        BackgroundGeolocation.start(); //triggers start on start event
+      }
+    });
+
+  }
+
+  componentWillUnmount(){
+    BackgroundGeolocation.events.forEach(event => BackgroundGeolocation.removeAllListeners(event));
   }
 
   selectCommunity(text){
