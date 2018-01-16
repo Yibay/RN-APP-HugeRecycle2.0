2c848b2fd20c832cb7f3812a0b287637e0bc478f
Yibay <514531536@qq.com>
2018-01-16 19:25:20 +0800

    1，定位小区 iOS、安卓 分平台开发；2、修改Android包名与原线上一致

--- android/app/src/main/java/com/huge_recycle_android/MainActivity.java
+++ android/app/src/main/java/com/huge_recycle_android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hugerecycle2_0;
+package com.huge_recycle_android;
 
 import com.facebook.react.ReactActivity;
 
--- app/containers/Location/CommunitySelector.js
+++ app/containers/Location/CommunitySelector.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ScrollView,TouchableWithoutFeedback, Alert } from 'react-native';
+import { StyleSheet, View, Text, ScrollView,TouchableWithoutFeedback } from 'react-native';
 
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import BackgroundGeolocation from 'react-native-mauron85-background-geolocation'; // 定位
-import { wgs84togcj02, gcj02tobd09 } from 'coordtransform'; // 坐标转换
+import PropTypes from 'prop-types';
 
 
 import { setLocation, setAutoLocationFlag } from '../../redux/actions/Location';
-import request from '../../util/request/request';
-import config from '../../util/request/config';
+import { locationCrossPlatform } from '../../HOC/locationCrossPlatform';
 
 
 class CommunitySelector extends Component{
+
+  static propTypes = {
+    LocateCommunities: PropTypes.array.isRequired, // 定位到的附近的小区 (HOC 提供)
+    communitySelected: PropTypes.shape({ // 当前选中小区 (Redux 提供)
+      communityName: PropTypes.string.isRequired
+    }),
+    setLocation: PropTypes.func.isRequired // 修改 当前选中小区的方法（Redux 提供）
+  };
 
   constructor(props){
     super(props);
 
     this.state = {
-      LocateCommunities: [],
       communitySelected: this.props.communitySelected
     };
   }
@@ -34,7 +39,7 @@ class CommunitySelector extends Component{
       <Text style={styles.title}>检测到这些小区在您周围</Text>
       <ScrollView style={styles.communityList}>
         <View style={styles.communityLayout}>
-          {this.state.LocateCommunities.map(
+          {this.props.LocateCommunities.map(
             (item, index) =>
               <TouchableWithoutFeedback key={index} onPress={() => this.selectCommunity(item)}>
                 <View style={[styles.community, this.state.communitySelected.communityName === item.communityName ? styles.communitySelected : styles.none]}>
@@ -55,111 +60,6 @@ class CommunitySelector extends Component{
         </View>
       </View>
     </View>);
-  }
-
-  componentDidMount(){
-
-    // 步骤1: 配置configure
-    BackgroundGeolocation.configure({
-      desiredAccuracy: 10,
-      stationaryRadius: 50,
-      distanceFilter: 50,
-      notificationTitle: 'Background tracking',
-      notificationText: 'enabled',
-      debug: true,
-      startOnBoot: false,
-      stopOnTerminate: false,
-      locationProvider: BackgroundGeolocation.DISTANCE_FILTER_PROVIDER, // location 供应商
-      interval: 10000,
-      fastestInterval: 5000,
-      activitiesInterval: 10000,
-      stopOnStillActivity: false,
-      maxLocations: 1, // db储存location数，就存1条id: 0的就好，节省存储空间
-      // url: 'http://192.168.81.15:3000/location',
-      httpHeaders: {
-        'X-FOO': 'bar'
-      }
-    });
-
-    BackgroundGeolocation.on('start', () => {
-      // 步骤3: 开启定位（监听）
-      console.log('[INFO] BackgroundGeolocation service has been started');
-    });
-
-    BackgroundGeolocation.on('authorization', (status) => {
-      // 步骤4: 授权定位（监听）
-      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
-      if (status !== BackgroundGeolocation.AUTHORIZED) {
-        Alert.alert('Location services are disabled', 'Would you like to open location settings?', [
-          { text: 'Yes', onPress: () => BackgroundGeolocation.showLocationSettings() },
-          { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
-        ]);
-      }
-    });
-
-    BackgroundGeolocation.on('location', (location) => {
-      // 步骤5: 获取定位结果 (监听)
-
-      // 地球坐标（WGS84）
-      console.log('------ on-location ------');
-      console.log('GPS坐标', location);
-      // 火星坐标 (GCJ-02)
-      let gcj02Location = wgs84togcj02(location.longitude, location.latitude);
-      console.log('火星坐标：', gcj02Location);
-      // 百度坐标 (BD-09)
-      let bd09Location = gcj02tobd09.apply(this, gcj02Location);
-      console.log('百度坐标：', bd09Location);
-
-      // 发送定位请求
-      request
-        .get(config.api.getLocateCommunity,{
-          longitude: bd09Location[0],
-          latitude: bd09Location[1]
-        })
-        .then(res => {
-          // 验证请求结果 是否正常（status: 0 正常，1 异常）
-          if(res && !res.status){
-            this.setState({
-              LocateCommunities: res.data
-            })
-          }
-        });
-
-      // 步骤6: 关闭 定位
-      BackgroundGeolocation.stop();
-      this.props.setAutoLocationFlag(false);
-    });
-
-    BackgroundGeolocation.on('stop', () => {
-      // 步骤6: 关闭 定位 (监听)
-      console.log('[INFO] BackgroundGeolocation service has been stopped');
-    });
-
-    BackgroundGeolocation.on('error', (error) => {
-      console.log('[ERROR] BackgroundGeolocation error:', error);
-    });
-
-    // 步骤2: 检测 状态
-    BackgroundGeolocation.checkStatus(status => {
-      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
-      console.log('[INFO] BackgroundGeolocation service has permissions', status.hasPermissions);
-      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
-
-      // you don't need to check status before start (this is just the example)
-      // 步骤3: 开启 定位
-      if (!status.isRunning) {
-        BackgroundGeolocation.start(); //triggers start on start event
-      }
-    });
-
-  }
-
-  componentWillUnmount(){
-    BackgroundGeolocation.events.forEach(event => BackgroundGeolocation.removeAllListeners(event));
-  }
-
-  componentDidUpdate(){
-    this.props.autoLocationFlag && BackgroundGeolocation.start();
   }
 
   selectCommunity(communitySelected){
@@ -253,4 +153,4 @@ function mapStateToProps(state){
 // 打包 actions 成obj
 const actionCreator = { setLocation, setAutoLocationFlag };
 
-export default connect(mapStateToProps, actionCreator)(CommunitySelector);
+export default connect(mapStateToProps, actionCreator)(locationCrossPlatform(CommunitySelector));
