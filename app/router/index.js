d65d555d464360f8552b3ae2b495882906ce78b9
Yibay <514531536@qq.com>
2018-03-02 08:50:22 +0800

    1、路由方案 变更；

--- app/router/index.js
+++ app/router/index.js
@@ -38,68 +38,69 @@ import MallNotOpen from '../pages/Home/Mall/MallNotOpen';
 import MallSettlement from "../pages/Home/Mall/MallSettlement";
 
 
-const AppRouter = () => (
-  <Router>
-    <Stack key='root'>
-      {/* 主入口页 */}
-      <Scene key='home' tabs={true} tabBarPosition='bottom' labelStyle={tabsStyle.labelStyle} activeTintColor='#000' inactiveTintColor='#000' tabBarStyle={tabsStyle.tabBarStyle} showLabel={false} >
+class AppRouter extends Component{
+
+  render(){
+    return <Router>
+      <Stack key='root'>
+        {/* 主入口页 */}
+        <Scene key='home' tabs={true} tabBarPosition='bottom' labelStyle={tabsStyle.labelStyle} activeTintColor='#000' inactiveTintColor='#000' tabBarStyle={tabsStyle.tabBarStyle} showLabel={false} >
+          {/* 回收分页: 回收首页 */}
+          <Scene key='recycle' icon={RecycleIcon} component={Recycle} hideNavBar={true} />
+          {/* 商城分页: 商城首页 */}
+          <Scene key='shoppingMall' icon={MallIcon} component={Mall} hideNavBar={true} />
+          {/* 我的分页 */}
+          <Scene key='mine' icon={MineIcon} component={Mine} hideNavBar={true} />
+        </Scene>
+
         {/* 回收分页 */}
-        <Scene key='tab1' icon={RecycleIcon}>
-          {/* 回收首页 */}
-          <Scene key='recycle' component={Recycle} hideNavBar={true} />
-          {/* 回收订单页 */}
-          <Scene key='recycleOrderPage' component={RecycleOrder} hideNavBar={true} />
-          {/* 呼叫成功 */}
-          <Scene key='callSuccessPage' component={CallSuccess} hideNavBar={true} />
-        </Scene>
+        {/* 回收订单页 */}
+        <Scene key='recycleOrderPage' component={RecycleOrder} hideNavBar={true} />
+        {/* 呼叫成功 */}
+        <Scene key='callSuccessPage' component={CallSuccess} hideNavBar={true} />
+
         {/* 商城分页 */}
-        <Scene key='tab2' icon={MallIcon}>
-          {/* 商城首页 */}
-          <Scene key='shoppingMall' component={Mall} hideNavBar={true} />
-          {/* 商城 商品分类页 */}
-          <Scene key='mallCategoryPage' component={MallCategory} hideNavBar={true} />
-          {/* 商城 商品购物车页 */}
-          <Scene key='mallCart' component={MallCart} hideNavBar={true} />
-          {/* 商城 商品搜索页 */}
-          <Scene key='mallSearch' component={MallSearch} hideNavBar={true} />
-          {/* 商城 便利店暂未开通页  */}
-          <Scene key='mallNotOpen' component={MallNotOpen} hideNavBar={true} />
-          {/* 商城 订单结算页 */}
-          <Scene key='mallSettlement' component={MallSettlement} hideNavBar={true} />
-        </Scene>
+        {/* 商城 商品分类页 */}
+        <Scene key='mallCategoryPage' component={MallCategory} hideNavBar={true} />
+        {/* 商城 商品购物车页 */}
+        <Scene key='mallCart' component={MallCart} hideNavBar={true} />
+        {/* 商城 商品搜索页 */}
+        <Scene key='mallSearch' component={MallSearch} hideNavBar={true} />
+        {/* 商城 便利店暂未开通页  */}
+        <Scene key='mallNotOpen' component={MallNotOpen} hideNavBar={true} />
+        {/* 商城 订单结算页 */}
+        <Scene key='mallSettlement' component={MallSettlement} hideNavBar={true} />
+
         {/* 我的分页 */}
-        <Scene key='tab3' icon={MineIcon}>
-          {/* 我的首页 */}
-          <Scene key='mine' component={Mine} hideNavBar={true} />
-          {/* 安全中心 */}
-          <Scene key='manageCustomerAccounts' component={ManageCustomerAccounts} hideNavBar={true} />
-          {/* 修改登录密码 */}
-          <Scene key='manageLoginPassword' component={ManageLoginPassword} hideNavBar={true} />
-          {/* 环保金余额 */}
-          <Scene key='customerScorePage' component={CustomerScore} hideNavBar={true} />
-        </Scene>
-      </Scene>
-      {/* 定位地址页 */}
-      <Scene key='locationPage' component={LocationPage} hideNavBar={true} />
-      {/* 手动输入小区 */}
-      <Scene key='locationManuallyPage' component={LocationManuallyPage} hideNavBar={true} />
-      {/* 新增地址页 */}
-      <Scene key='addressAddPage' component={AddressAdd} hideNavBar={true} />
-      {/* 选择地址页 */}
-      <Scene key='addressSelectionPage' component={AddressSelection} hideNavBar={true} />
-      {/* 编辑地址 */}
-      <Scene key='addressEditPage' component={AddressEdit} hideNavBar={true} />
-      {/* 环保记录 */}
-      <Scene key='environmentalRecordPage' component={RecycleRecord} hideNavBar={true} />
-      {/* 评价虎哥 */}
-      <Scene key='recycleEvaluationPage' component={RecycleEvaluation} hideNavBar={true} />
-      {/* 轮播简介页 */}
-      <Scene key='intro' component={Intro} hideNavBar={true} />
-      {/* 登录页 */}
-      <Scene key='login' component={Login} hideNavBar={true} />
-    </Stack>
-  </Router>
-);
+        {/* 安全中心 */}
+        <Scene key='manageCustomerAccounts' component={ManageCustomerAccounts} hideNavBar={true} />
+        {/* 修改登录密码 */}
+        <Scene key='manageLoginPassword' component={ManageLoginPassword} hideNavBar={true} />
+        {/* 环保金余额 */}
+        <Scene key='customerScorePage' component={CustomerScore} hideNavBar={true} />
+
+        {/* 定位地址页 */}
+        <Scene key='locationPage' component={LocationPage} hideNavBar={true} />
+        {/* 手动输入小区 */}
+        <Scene key='locationManuallyPage' component={LocationManuallyPage} hideNavBar={true} />
+        {/* 新增地址页 */}
+        <Scene key='addressAddPage' component={AddressAdd} hideNavBar={true} />
+        {/* 选择地址页 */}
+        <Scene key='addressSelectionPage' component={AddressSelection} hideNavBar={true} />
+        {/* 编辑地址 */}
+        <Scene key='addressEditPage' component={AddressEdit} hideNavBar={true} />
+        {/* 环保记录 */}
+        <Scene key='environmentalRecordPage' component={RecycleRecord} hideNavBar={true} />
+        {/* 评价虎哥 */}
+        <Scene key='recycleEvaluationPage' component={RecycleEvaluation} hideNavBar={true} />
+        {/* 轮播简介页 */}
+        <Scene key='intro' component={Intro} hideNavBar={true} />
+        {/* 登录页 */}
+        <Scene key='login' component={Login} hideNavBar={true} />
+      </Stack>
+    </Router>
+  }
+}
 
 // 因为对屏幕做了适配，所以要对导航的默认样式做些调整
 const styles = StyleSheet.create({
@@ -137,3 +138,4 @@ const tabsStyle = {
 
 // 屏宽适配, 登录状态管理相关数据, 地址管理相关数据
 export default adaptLayoutWidth(initIdentityToken(locationManage(AppRouter)));
+// export default adaptLayoutWidth(initIdentityToken(AppRouter));
