568b26d8d0499c8b4e92eac913b76f7a9e561812
Yibay <514531536@qq.com>
2018-04-12 15:10:34 +0800

    1、虎哥资讯（详情页）webView 降低分辨率，提升 外链网页 字体大小

--- app/pages/Home/Mine/HugeInformationDetail.js
+++ app/pages/Home/Mine/HugeInformationDetail.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
-import {StyleSheet, View, WebView} from 'react-native';
+import {StyleSheet, View, WebView, Dimensions} from 'react-native';
 
 import PropTypes from 'prop-types';
 
 
 import Header from "../../../components/Header/Header";
 
+let scale = 2; // webView 放大倍数，因img有自适应，所以 主影响 字体大小
+let {width, height} = Dimensions.get('window');
+let webViewHeight = height / width * 750 - 128; // Header 高度
 
 class HugeInformationDetail extends Component{
 
@@ -14,10 +17,12 @@ class HugeInformationDetail extends Component{
   };
 
   render(){
-    console.log(this.props.url);
+    // 因为 全局适配 分辨率太高，网页字体显示小，此处 降低分辨率，放大字体。
     return <View style={styles.container}>
       <Header title='虎哥资讯'/>
-      <WebView source={{uri: this.props.url}}/>
+      <View style={styles.webView}>
+        <WebView source={{uri: this.props.url}}/>
+      </View>
     </View>
   }
 }
@@ -25,6 +30,17 @@ class HugeInformationDetail extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  webView: {
+    width: 750 / scale,
+    height: webViewHeight / scale,
+    transform: [
+      {translateX: 750 / scale * -.5},
+      {translateY: webViewHeight / scale * -.5},
+      {scale},
+      {translateX: 750 / scale * .5},
+      {translateY: webViewHeight / scale * .5},
+    ]
   }
 });
 
